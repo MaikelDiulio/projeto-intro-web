82430b00e69d857adf04b0df969d04fbe29ff74e
maikeldiulio <maikeltecba@gmail.com>
2022-09-23 12:06:16 -0300

    Criei as constantes de cada item e fiz a média

---
+++ index.js
@@ -0,0 +1,21 @@
+const nome1 = "SUPRA"
+const marca1 ="TOYOTA"
+const potencia1 = 330
+const eletrico1 = false
+const motorizacao1 =["6 cilindros em linha", "turbo", "gasolina","441 Nm de torque"]
+
+const nome2 = "GOLF R32"
+const marca2 = "VOLKSWAGEN"
+const potencia2 = 200
+const eletrico2 =false
+const motorizacao2 =["V6", "turbo", "Gasolina", , "27kgfm de Torque" ]
+
+const nome3 = "Model S"
+const marca3 ="TESLA"
+const potencia3 = 1034
+const eletrico3 = true
+const motorizacao3 =["3 motores, Ambos Eletricos", "145,2 kgfm de torque" ]
+
+const media = ((potencia1+potencia2+potencia3)/3)
+console.log(`O valor da média é ${media}`)
+// console.log("Essa é a média dos laçamentos dos carros " + media)
